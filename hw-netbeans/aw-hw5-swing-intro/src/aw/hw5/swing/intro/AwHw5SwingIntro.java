596e3a9c4e0a5dedf239e5e6f9fd31ea01ff7c1a
Andrew Wei <Andrew@Andrew-Wei.local>
2015-12-14 10:29:59 +0800

    Added Action Listeners for current video number
    Add and delete changes video number, but can’t save tracks yet.

---
+++ hw-netbeans/aw-hw5-swing-intro/src/aw/hw5/swing/intro/AwHw5SwingIntro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aw.hw5.swing.intro;
+
+/**
+ *
+ * @author Andrew
+ */
+public class AwHw5SwingIntro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
